ed7fb091ca49ba9a8a781d03e78ec11a6a6962e3
Matei-oss <imateiu@DEGL4J3Z2.emea.steelcase.net>
2022-01-09 15:14:48 +0200

    changed space entity

--- WebApplication1/WebApplication1/Entities/Space.cs
+++ WebApplication1/WebApplication1/Entities/Space.cs
@@ -7,6 +7,7 @@ namespace DeviceManagerBackend.Entities
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Type { get; set; }
         public List<Room> Rooms { get; set; } = new List<Room>();
     }
 }
