dc87d6314b5899c15a460e0df680deafc2f3f4b2
Matei-oss <imateiu@DEGL4J3Z2.emea.steelcase.net>
2022-01-20 14:01:18 +0200

    added login module

--- ClientApp/ClientApp/src/app/app-routing.module.ts
+++ ClientApp/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { LoginModule } from './login/login.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ ClientApp/ClientApp/src/app/login/login.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginRoutingModule } from './login-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    LoginPageComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule
+  ],
+  exports: [
+    LoginPageComponent
+  ],
+  entryComponents : [
+    LoginPageComponent
+  ]
+})
+export class LoginModule { }
