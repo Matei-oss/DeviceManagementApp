a1abf8ff05283ea6d292feeb6406ffc1b83b985b
Matei-oss <andrei_mateiu1996@live.com>
2022-01-24 13:16:17 +0200

    added form for login

--- ClientApp/ClientApp/src/app/login/login-page/login-page.component.ts
+++ ClientApp/ClientApp/src/app/login/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -7,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(private readonly fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+   }
 
+  submitForm(){
+    if (this.form.valid){
+      console.log(this.form.getRawValue())
+    } else {
+      console.log('There is a problem with the form!');
+    }
+    
+  }
   ngOnInit(): void {
   }
 
--- ClientApp/ClientApp/src/app/login/login.module.ts
+++ ClientApp/ClientApp/src/app/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginRoutingModule } from './login-routing.module';
+import {  ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +12,8 @@ import { LoginRoutingModule } from './login-routing.module';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    ReactiveFormsModule,
   ],
   exports: [
     LoginPageComponent
